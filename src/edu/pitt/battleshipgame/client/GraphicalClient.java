75f50bc9b723c273670e7b57e20d169175c5c506
Ben Anderson <ben.anderson19@gmail.com>
2017-02-14 14:21:03 -0500

    working on getting menu bar in place

--- src/edu/pitt/battleshipgame/client/GraphicalClient.java
+++ src/edu/pitt/battleshipgame/client/GraphicalClient.java
@@ -7,45 +7,123 @@ package edu.pitt.battleshipgame.client;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
-/**
- *
- * @author ben
- */
-public class GraphicalClient extends Application {
+public class GraphicalClient extends Application
+{
+    private Scene scene;
+    private Stage primaryStage;
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage)
+    {   
+        this.primaryStage = primaryStage;
+        initialize();
+        primaryStage.setTitle("Battleship");
+        primaryStage.setScene(this.scene);
+        primaryStage.setMinHeight(250);
+        primaryStage.setMinWidth(400);
         primaryStage.show();
+    }
+    
+    private void initialize()
+    {
+        GridPane grid = GenerateGrid();
+        grid.getChildren().add(GenerateMenuBar());
+        this.scene = GenerateScene(grid);
+    }
+    
+    private Scene GenerateScene(Parent parent)
+    {
+        Scene scene = new Scene(parent, 800, 450);
+        return scene;
+    }
+    
+    private GridPane GenerateGrid()
+    {
+        GridPane grid = new GridPane();
+        RowConstraints rowConstraints = new RowConstraints();
+        rowConstraints.setPercentHeight(10);
+        grid.getRowConstraints().add(rowConstraints);
+        rowConstraints.setPercentHeight(80);
+        grid.getRowConstraints().add(rowConstraints);
+        rowConstraints.setPercentHeight(10);
+        grid.getRowConstraints().add(rowConstraints);
+        return grid;
+    }
+    
+    private GridPane GeneratePlacementButtonGrid()
+    {
+        GridPane grid = new GridPane();
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setPercentWidth(20);
+        for (int i = 0; i < 5; i++)
+        {
+            grid.getColumnConstraints().add(columnConstraints);
+        }
+        return grid;
+    }
+    
+    private Button GenerateButton(String text)
+    {
+        Button button = new Button();
+        button.setText(text);
+        return button;
+    }
+    
+    private GridPane GenerateBoardPanel()
+    {
+        GridPane grid = new GridPane();
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setPercentWidth(50);
+        for (int i = 0; i < 2; i++)
+        {
+            grid.getColumnConstraints().add(columnConstraints);
+        }
+        return grid;
+    }
+    
+    private MenuBar GenerateMenuBar()
+    {
+        MenuItem surrender = new MenuItem("Surrender");
+        surrender.setOnAction(this::surrender);
+        
+        MenuItem quit = new MenuItem("Quit");
+        quit.setOnAction(this::quit);
+        
+        Menu file = new Menu("File");
+        file.getItems().addAll(surrender, quit);
+        
+        MenuBar menuBar = new MenuBar();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+        menuBar.getMenus().add(file);
+        return menuBar;
+    }
+    
+    private void surrender(ActionEvent e)
+    {
+        //TODO
+    }
+    
+    private void quit(ActionEvent e)
+    {
+        //TODO
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
-    
 }
