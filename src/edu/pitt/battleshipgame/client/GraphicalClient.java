ec24f2dadeff914aed7412a339fdb45ca7236c3c
Ben Anderson <ben.anderson19@gmail.com>
2017-02-16 06:27:03 -0500

    got the boards showing up

--- src/edu/pitt/battleshipgame/client/GraphicalClient.java
+++ src/edu/pitt/battleshipgame/client/GraphicalClient.java
@@ -8,19 +8,16 @@ package edu.pitt.battleshipgame.client;
 import edu.pitt.battleshipgame.common.ships.Ship;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
@@ -29,6 +26,8 @@ public class GraphicalClient extends Application
     private Scene scene;
     private Stage primaryStage;
     private GridPane grid;
+    private BorderPane ourBoard;
+    private BorderPane theirBoard;
     
     @Override
     public void start(Stage primaryStage)
@@ -46,11 +45,12 @@ public class GraphicalClient extends Application
     {
         this.grid = GenerateGrid();
         this.grid.add(GeneratePlacementButtonGrid(), 0, 2);
+        GridPane boardPane = GenerateBoardPane();
+        this.grid.add(boardPane, 0, 1);
         BorderPane masterPane = GenerateMasterPane();
         masterPane.setTop(GenerateMenuBar());
         masterPane.setCenter(this.grid);
         this.scene = GenerateScene(masterPane);
-        //this.scene = GenerateScene(this.grid);
     }
     
     private Scene GenerateScene(Parent parent)
@@ -93,6 +93,9 @@ public class GraphicalClient extends Application
         {
             grid.getColumnConstraints().add(columnConstraints);
         }
+        RowConstraints rowConstraint = new RowConstraints();
+        rowConstraint.setPercentHeight(100);
+        grid.getRowConstraints().add(rowConstraint);
         
         int i = 0;
         for (Ship.ShipType ship : Ship.ShipType.values())
@@ -115,11 +118,10 @@ public class GraphicalClient extends Application
         GridPane.setFillWidth(button, true);
         GridPane.setFillHeight(button, true);
         button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        button.setAlignment(Pos.BOTTOM_CENTER);
         return button;
     }
     
-    private GridPane GenerateBoardPanel()
+    private GridPane GenerateBoardPane()
     {
         GridPane grid = new GridPane();
         ColumnConstraints columnConstraints = new ColumnConstraints();
@@ -128,7 +130,53 @@ public class GraphicalClient extends Application
         {
             grid.getColumnConstraints().add(columnConstraints);
         }
+        RowConstraints rowConstraint = new RowConstraints();
+        rowConstraint.setPercentHeight(100);
+        grid.getRowConstraints().add(rowConstraint);
+        
+        this.ourBoard = GenerateBoard("Your Board");
+        this.theirBoard = GenerateBoard("Opponent's Board");
+        grid.add(this.ourBoard, 0, 0);
+        grid.add(this.theirBoard, 1, 0);
+        
+        grid.setGridLinesVisible(true);
         return grid;
+    }
+    
+    private BorderPane GenerateBoard(String title)
+    {
+        BorderPane pane = new BorderPane();
+        pane.setTop(new Label(title));
+        
+        GridPane board = new GridPane();
+        ConstrainBoard(board);
+        LabelBoard(board);
+        pane.setCenter(board);
+        
+        return pane;
+    }
+    
+    private void LabelBoard(GridPane board)
+    {
+        char column = 'A';
+        for (int i = 1; i <= 10; i++, column++)
+        {
+            board.add(new Label(String.valueOf(i)), 0, i);
+            board.add(new Label(String.valueOf(column)), i, 0);
+        }
+    }
+    
+    private void ConstrainBoard(GridPane board)
+    {
+        RowConstraints rowConstraints = new RowConstraints();
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        rowConstraints.setPercentHeight(10);
+        columnConstraints.setPercentWidth(10);
+        for (int i = 0; i < 11; i++)
+        {
+            board.getRowConstraints().add(rowConstraints);
+            board.getColumnConstraints().add(columnConstraints);
+        }
     }
     
     private MenuBar GenerateMenuBar()
